a61cfc451094f43963637a36ae865082cf9b8571
gpsiegel <gpsiegel@hotmail.com>
2021-08-18 16:32:04 -0500

    change front end

--- front_end-MAIN/app.py
+++ front_end-MAIN/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from ddb_scan import ddb_return
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 api_result = ddb_return()
 
-@app.route('/')
+@application.route('/')
 def index():
     return api_result
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
